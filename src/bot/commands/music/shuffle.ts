5197f8eff56c6b7b20dbb5de3fd24fbd254da2d1
Crawl <icrawltogo@gmail.com>
2018-11-21 08:25:41 +0100

    feat: add shuffle

---
+++ src/bot/commands/music/shuffle.ts
@@ -0,0 +1,26 @@
+import { Command } from 'discord-akairo';
+import { Message } from 'discord.js';
+
+export default class ShuffleCommand extends Command {
+	public constructor() {
+		super('shuffle', {
+			aliases: ['shuffle', '🔀'],
+			description: {
+				content: 'Shuffles the queue.'
+			},
+			category: 'music',
+			channel: 'guild',
+			ratelimit: 2
+		});
+	}
+
+	public async exec(message: Message) {
+		if (!message.member.voice || !message.member.voice.channel) {
+			return message.util!.reply('You have to be in a voice channel first, silly.');
+		}
+		const queue = this.client.music.queues.get(message.guild.id);
+		await queue.shuffle();
+
+		return message.util!.send('Shuffled the queue.');
+	}
+}
