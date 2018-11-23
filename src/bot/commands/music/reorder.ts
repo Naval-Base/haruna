21754590073a56363324360be4a72fb3383f9ecc
1computer1 <mineox100@gmail.com>
2018-11-22 21:50:14 -0500

    Cleanup reorder command

--- src/bot/commands/music/reorder.ts
+++ src/bot/commands/music/reorder.ts
@@ -3,22 +3,31 @@ import { Message } from 'discord.js';
 import { stripIndents } from 'common-tags';
 
 interface SingleAction {
-	kind?: 'single';
+	kind: 'single';
 	num: number;
 }
 
 interface SliceAction {
-	kind?: 'slice';
+	kind: 'slice';
 	from: number;
 	to: number;
 	reverse: boolean;
 }
 
 interface SpreadAction {
-	kind?: 'spread';
+	kind: 'spread';
 }
 
-type Actions = SingleAction | SliceAction | SpreadAction; // tslint:disable-line
+type Action = SingleAction | SliceAction | SpreadAction;
+
+interface OrderingMatch {
+	sliceFrom?: string;
+	sliceTo?: string;
+	singleNum?: string;
+	spread?: string;
+}
+
+const orderingRegex = /\s*(?<sliceFrom>\d+)-(?<sliceTo>\d+)\s*|\s*(?<singleNum>\d+)\s*|\s*(?<spread>\*)\s*/g;
 
 export default class ReorderCommand extends Command {
 	constructor() {
@@ -28,11 +37,11 @@ export default class ReorderCommand extends Command {
 				content: stripIndents`
 					Reorders the current queue.
 					A number means that the song at that number currently will be moved to that position.
-					A dash between two numbers means to move all the songs, starting from first number to the second number.
-					A \\* means to spread the remaining songs out (multiple \\* will split it evenly).
+					A '-' between two numbers means to move all the songs, starting from first number to the second number.
+					A '\\*' means to spread the remaining songs out (multiple '\\*' will split it evenly).
 				`,
 				usage: '<ordering>',
-				examples: ['1-3 7 *', '1 2 3 *']
+				examples: ['1-3 7 *', '1 2 3 *', '10-7 * 1 2 3 3 * 10-7']
 			},
 			category: 'music',
 			channel: 'guild',
@@ -46,31 +55,31 @@ export default class ReorderCommand extends Command {
 		});
 	}
 
-	public async exec(message: Message, { ordering }: { ordering: any }) {
+	public async exec(message: Message, { ordering }: { ordering: string | null }) {
 		if (!ordering) {
 			return message.util!.reply('You have to supply a new order for the songs.');
 		}
-		const orderingRegex = /\s*(\d+)-(\d+)\s*|\s*(\d+)\s*|\s*(\*)\s*/g;
 		const orderingMatch = ordering.match(orderingRegex);
 		if (!orderingMatch || orderingMatch.join('').length !== ordering.length) {
 			return message.util!.reply('You have to supply a valid new order for the songs.');
 		}
 
 		const queue = this.client.music.queues.get(message.guild.id);
-		const queueLength = await queue.length(); // tslint:disable-line
-		const actions: Actions[] = [];
+		const queueLength = await queue.length();
+		const actions: Action[] = [];
 		let match;
 		while ((match = orderingRegex.exec(ordering)) !== null) { // tslint:disable-line
-			if (match[1]) {
-				let from = Number(match[1]) - 1;
-				let to = Number(match[2]) - 1;
+			const groups: OrderingMatch = match.groups!;
+			if (groups.sliceFrom && groups.sliceTo) {
+				let from = Number(groups.sliceFrom) - 1;
+				let to = Number(groups.sliceTo) - 1;
 				let reverse = false;
 				if (to < from) {
 					[from, to] = [to, from];
 					reverse = true;
 				}
 
-				if (from >= queueLength || to >= queueLength) {
+				if (from >= queueLength || to >= queueLength || from < 0 || to < 0) {
 					return message.util!.reply('Some song numbers were out of bound.');
 				}
 
@@ -80,9 +89,9 @@ export default class ReorderCommand extends Command {
 					to,
 					reverse
 				});
-			} else if (match[3]) {
-				const num = Number(match[3]) - 1;
-				if (num >= queueLength) {
+			} else if (groups.singleNum) {
+				const num = Number(groups.singleNum) - 1;
+				if (num >= queueLength || num < 0) {
 					return message.util!.reply('Some song numbers were out of bound.');
 				}
 
@@ -98,20 +107,20 @@ export default class ReorderCommand extends Command {
 		}
 
 		const tracks = await queue.tracks();
-		const numbers = new Set(Array.from({ length: queueLength }, (_, i) => i));
+		const unusedIndices = new Set(Array.from({ length: queueLength }, (_, i) => i));
 		const newTracks = [];
 		for (const action of actions) {
 			switch (action.kind) {
 				case 'single':
 					newTracks.push(tracks[action.num]);
-					numbers.delete(action.num);
+					unusedIndices.delete(action.num);
 					break;
 				case 'slice':
 					const slice = tracks.slice(action.from, action.to + 1);
 					if (action.reverse) slice.reverse();
 					newTracks.push(...slice);
 					for (let i = action.from; i <= action.to; i++) {
-						numbers.delete(i);
+						unusedIndices.delete(i);
 					}
 					break;
 				case 'spread':
@@ -124,11 +133,11 @@ export default class ReorderCommand extends Command {
 
 		const spreadAmount = actions.filter(a => a.kind === 'spread').length;
 		if (spreadAmount) {
-			const sliceSize = Math.ceil(numbers.size / spreadAmount);
-			const numbersA = Array.from(numbers);
+			const sliceSize = Math.ceil(unusedIndices.size / spreadAmount);
+			const unusedTracks = Array.from(unusedIndices, n => tracks[n]);
 			for (let i = 0; i < newTracks.length; i++) {
 				if (newTracks[i] === '*') {
-					newTracks.splice(i, 1, ...numbersA.splice(0, sliceSize).map(i => tracks[i]));
+					newTracks.splice(i, 1, ...unusedTracks.splice(0, sliceSize));
 				}
 			}
 		}
