266a55af23367f1152b0fc867bb32b552af93393
iCrawl <icrawltogo@gmail.com>
2019-09-02 19:39:54 +0200

    fix: remove past tense from events

---
+++ src/bot/listeners/client/shardDisconnect.ts
@@ -0,0 +1,17 @@
+import { Listener } from 'discord-akairo';
+
+export default class ShardDisconnectListener extends Listener {
+	public constructor() {
+		super('shardDisconnect', {
+			emitter: 'client',
+			event: 'shardDisconnect',
+			category: 'client'
+		});
+	}
+
+	public exec(event: any, id: number): void {
+		this.client.logger.warn(`[SHARD ${id} DISCONNECT] Ugh...I'm sorry...but, a loss is a loss... (${event.code})`, event);
+		this.client.promServer.close();
+		this.client.logger.info(`[SHARD ${id} DISCONNECT][METRICS] Metrics server closed.`);
+	}
+}
--- src/bot/listeners/client/shardResume.ts
+++ src/bot/listeners/client/shardResume.ts
@@ -2,16 +2,16 @@ import { Listener } from 'discord-akairo';
 
 export default class ShardResumeListener extends Listener {
 	public constructor() {
-		super('shardResumed', {
+		super('shardResume', {
 			emitter: 'client',
-			event: 'shardResumed',
+			event: 'shardResume',
 			category: 'client'
 		});
 	}
 
 	public exec(id: number): void {
-		this.client.logger.info(`[SHARD ${id} RESUMED] Alright, next time I'll--Eh...again...?`);
+		this.client.logger.info(`[SHARD ${id} RESUME] Alright, next time I'll--Eh...again...?`);
 		this.client.promServer.listen(5501);
-		this.client.logger.info(`[SHARD ${id} RESUMED][METRICS] Metrics listening on 5501`);
+		this.client.logger.info(`[SHARD ${id} RESUME][METRICS] Metrics listening on 5501`);
 	}
 }
